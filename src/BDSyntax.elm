548000dfea48dea071458fc8679542338ad9e858
wangfusen <201250185@smail.nju.edu.cn>
2024-03-21 16:37:17 +0800

    define types of template

--- src/BDSyntax.elm
+++ src/BDSyntax.elm
@@ -1,4 +1,5 @@
 module BDSyntax exposing (..)
+import Html.Attributes exposing (default)
 
 type alias Info = String
 
@@ -77,9 +78,22 @@ type Expr
     | EFix WS Expr
     | EBTuple WS Expr Expr
     | ETTuple WS Expr Expr Expr
+    | StrTpl Template
     | EHtml WS String Expr Expr Expr
     | EToStr WS Expr
     | EError Info
+
+type Template
+    = TCons TplPart Template
+    | TNil
+
+type TplPart
+    = TplStr Expr
+    | TplExpr Expr
+    | TplSet WS Pattern Expr
+    | TplIf WS Expr Template Template
+    | TplForeach WS Pattern Expr Template
+    | Splice Expr
 
 type Bop = Add | Sub | Mul | Div | Eq | Lt | Gt | Le | Ge | And | Or | Cat | DDiv
 type Uop = Not | Neg
